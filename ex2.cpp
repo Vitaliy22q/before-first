256a204b2a8150692953ac39dc215e3f669333ce
Vitaliy22q <Vitaliy22q@list.ru>
2016-03-12 12:16:34 +0200

    Added files via upload

---
+++ ex2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+
+
+int main()
+{
+	int a,b,x;
+	printf ("a = ");
+	scanf ("%d",&a);
+	printf("\nb = ");
+	scanf("%d",&b);
+	x=a+b;
+	printf ("%d",x);
+	return 0;
+	
+
+}
+
