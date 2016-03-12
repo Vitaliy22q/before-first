51f3678a1d94d59fc73658f4a22be321207053d1
Vitaliy22q <Vitaliy22q@list.ru>
2016-03-12 12:13:11 +0200

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <stdio.h>
 
-int main () {
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Good night, good night.\n");
+	printf("My little child.\t");
+	printf("Wake up gay and bright\n");
+	printf("In the morning light.\n");
+	return 0;
 	
-	int x = 7;
-	int y = x*x;
-	printf("%i", y);
-	getchar ();
 }
-
-
-
